5557faee3a360710c68ae4351aa51746463170ae
EduardoBackesDev <eduardo.programacao@hotmail.com>
2023-12-13 15:40:01 -0300

    alksj

---
+++ Js/script.js
@@ -0,0 +1,22 @@
+// Inicio Script para redirecionamento
+
+
+function redirecionamento(){ 
+    var appURL = 'https://apps.apple.com/br/app/appbarber-cliente/id6450795073'
+
+    window.open(appURL, '_blank');
+}
+
+function whats (){
+    var whatsURL = 'https://api.whatsapp.com/send?phone=555492367901&text=Ol%C3%A1,%20gostaria%20de%20agendar%20um%20corte!'
+
+    window.open(whatsURL, '_blank');
+
+}
+
+
+
+
+
+
+// Fim script para redirecionamento
